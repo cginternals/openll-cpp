c83f47656e0651c89dd39623e1f4f45bd9cb4bb8
Stefan Buschmann <stefan.buschmann@hpi.de>
2018-04-07 12:37:42 +0200

    Adjust comments for Alignment and LineAnchor

--- source/openll/include/openll/Alignment.h
+++ source/openll/include/openll/Alignment.h
@@ -11,13 +11,13 @@ namespace openll
 
 /**
 *  @brief
-*    Alignment enum class for glyphs in an annotation space.
+*    Horizontal text alignment for glyphs in an annotation space
 */
 enum class Alignment : unsigned char
 {
-    LeftAligned, ///< Left align glyphs
-    Centered,    ///< Center align glyphs
-    RightAligned ///< Right align glyphs
+    LeftAligned, ///< Left align text at the origin
+    Centered,    ///< Center align text at the origin
+    RightAligned ///< Right align text at the origin
 };
 
 
@@ -30,15 +30,14 @@ namespace std
 
 /**
 *  @brief
-*    Hash specialization for Alignment enum class.
+*    Hash specialization for Alignment enum
 *
-*    Enables the use of Alignment as key type of the unordered collection types.
+*    Enables the use of Alignment as a key type of the unordered collection types.
 */
 template<>
 struct hash<openll::Alignment>
 {
-    std::hash<unsigned char>::result_type operator()(
-        const openll::Alignment & arg) const
+    std::hash<unsigned char>::result_type operator()(const openll::Alignment & arg) const
     {
         std::hash<unsigned char> hasher;
         return hasher(static_cast<unsigned char>(arg));
--- source/openll/include/openll/LineAnchor.h
+++ source/openll/include/openll/LineAnchor.h
@@ -9,9 +9,16 @@ namespace openll
 {
 
 
+/**
+*  @brief
+*    Vertical anchor point for text
+*/
 enum class LineAnchor : unsigned char
 {
-    Ascent, Center, Baseline, Descent
+    Ascent,   ///< Anchor text to the beginning (top) of the glyph
+    Center,   ///< Anchor text to the center of the glyph
+    Baseline, ///< Anchor text to the baseline of the glyph
+    Descent   ///< Anchor text to the end (bottom) of the glyph
 };
 
 
@@ -22,11 +29,16 @@ namespace std
 {
 
 
+/**
+*  @brief
+*    Hash specialization for LineAnchor enum
+*
+*    Enables the use of LineAnchor as a key type of the unordered collection types.
+*/
 template<>
 struct hash<openll::LineAnchor>
 {
-    std::hash<unsigned char>::result_type operator() (
-        const openll::LineAnchor & arg) const
+    std::hash<unsigned char>::result_type operator() (const openll::LineAnchor & arg) const
     {
         std::hash<unsigned char> hasher;
         return hasher(static_cast<unsigned char>(arg));
